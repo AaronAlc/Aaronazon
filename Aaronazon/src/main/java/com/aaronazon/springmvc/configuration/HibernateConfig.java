34d843104fba186570f1c49a5ab2f96a8ce1d390
Aaron <aalcoser85@gmail.com>
2019-04-04 14:13:43 -0500

    added some classes

---
+++ Aaronazon/src/main/java/com/aaronazon/springmvc/configuration/HibernateConfig.java
@@ -0,0 +1,9 @@
+package com.aaronazon.springmvc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configurationion
+@EnableTransactionManagement
+public class HibernateConfig {
+
+}
